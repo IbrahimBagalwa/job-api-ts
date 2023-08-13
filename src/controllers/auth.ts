46e8e706ad45cf8fbd2f16da6fa851c28974efba
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-13 10:27:24 +0200

    feat: make async all the controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,17 +1,15 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-function register(req: Request, res: Response) {
-  res
-    .status(StatusCodes.CREATED)
-    .json({
-      success: true,
-      status: StatusCodes.CREATED,
-      message: "User created successfully",
-    });
+async function register(req: Request, res: Response) {
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    status: StatusCodes.CREATED,
+    message: "User created successfully",
+  });
 }
 
-function login(req: Request, res: Response) {
+async function login(req: Request, res: Response) {
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
--- src/controllers/job.ts
+++ src/controllers/job.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-function createJob(req: Request, res: Response) {
+async function createJob(req: Request, res: Response) {
   res.status(StatusCodes.CREATED).json({
     success: true,
     status: StatusCodes.CREATED,
     message: "Job created successfully",
   });
 }
-function getAllJobs(req: Request, res: Response) {
+async function getAllJobs(req: Request, res: Response) {
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
@@ -16,21 +16,21 @@ function getAllJobs(req: Request, res: Response) {
   });
 }
 
-function getJob(req: Request, res: Response) {
+async function getJob(req: Request, res: Response) {
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
     message: "Retrieved job successfuly",
   });
 }
-function updateJob(req: Request, res: Response) {
+async function updateJob(req: Request, res: Response) {
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
     message: "Job updated successfully",
   });
 }
-function deleteJob(req: Request, res: Response) {
+async function deleteJob(req: Request, res: Response) {
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
