ae871a35588d849eba18eff91b6dc70ffb837a5b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-13 22:46:32 +0200

    feat: validation error

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import { CustomErrorApi } from "../error";
 import { StatusCodes } from "http-status-codes";
 
 interface ErrorHandle extends Error {
@@ -7,6 +6,7 @@ interface ErrorHandle extends Error {
   message: string;
   code: number;
   keyValue: object;
+  errors: object;
 }
 function errorHandler(
   err: ErrorHandle,
@@ -20,11 +20,6 @@ function errorHandler(
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || "Something went wrong, please try again later.",
   };
-  if (err instanceof CustomErrorApi) {
-    return res
-      .status(err.statusCode)
-      .json({ success: true, status: err.statusCode, message: err.message });
-  }
 
   if (err.code && err.code === 11000) {
     customError.message = `Duplicate entered for ${Object.keys(
@@ -32,13 +27,21 @@ function errorHandler(
     )}, field please choose another value`;
     customError.statusCode = StatusCodes.BAD_REQUEST;
   }
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    err,
-  });
-  // res.status(customError.statusCode).json({
-  //   success: false,
-  //   status: customError.statusCode,
-  //   message: customError.message,
+
+  if (err.name && err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(" and ");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+  //   err,
   // });
+  res.status(customError.statusCode).json({
+    success: false,
+    status: customError.statusCode,
+    message: customError.message,
+  });
 }
 export default errorHandler;
