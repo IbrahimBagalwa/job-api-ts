257e644a75d06eaf5f87a4c25b13f81630ed0813
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-13 19:31:53 +0200

    feat: get jobs

--- src/controllers/job.ts
+++ src/controllers/job.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import Job from "../models/Job";
+import { NotFoundError } from "../error";
 
 async function createJob(req: Request, res: Response) {
   req.body.createdBy = req.user.userId;
@@ -13,19 +14,31 @@ async function createJob(req: Request, res: Response) {
     job,
   });
 }
+
 async function getAllJobs(req: Request, res: Response) {
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
     message: "Jobs retrieved successfully",
+    jobs,
   });
 }
 
 async function getJob(req: Request, res: Response) {
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError("No Job found");
+  }
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
     message: "Retrieved job successfuly",
+    job,
   });
 }
 async function updateJob(req: Request, res: Response) {
