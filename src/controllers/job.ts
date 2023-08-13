178b505836822992f998ae27f301c497ac4e2625
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-13 19:59:42 +0200

    feat: implement all funtionalities in job controller

--- src/controllers/job.ts
+++ src/controllers/job.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import Job from "../models/Job";
-import { NotFoundError } from "../error";
+import { BadRequestError, NotFoundError } from "../error";
 
 async function createJob(req: Request, res: Response) {
   req.body.createdBy = req.user.userId;
@@ -32,7 +32,7 @@ async function getJob(req: Request, res: Response) {
   } = req;
   const job = await Job.findOne({ _id: jobId, createdBy: userId });
   if (!job) {
-    throw new NotFoundError("No Job found");
+    throw new NotFoundError(`No Job found with this id ${jobId}`);
   }
   res.status(StatusCodes.OK).json({
     success: true,
@@ -42,13 +42,41 @@ async function getJob(req: Request, res: Response) {
   });
 }
 async function updateJob(req: Request, res: Response) {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  if (!company || !position) {
+    throw new BadRequestError("company and position are required");
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!job) {
+    throw new NotFoundError(`No Job found with this id ${jobId}`);
+  }
+
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
     message: "Job updated successfully",
+    job,
   });
 }
 async function deleteJob(req: Request, res: Response) {
+  const {
+    user: { userId },
+    params: { jobId },
+  } = req;
+  const job = await Job.findByIdAndRemove({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No Job found with this id ${jobId}`);
+  }
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
