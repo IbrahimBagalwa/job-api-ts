d9eacee69f4046b9c8ec041e092d87613fbbfbbe
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-16 11:06:26 +0200

    feat: update the documentation

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,8 @@ const swaggerDocumentation = YAML.load("./swagger.yaml");
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
-    windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 7, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     // store: ... , // Use an external store for more precise rate limiting
